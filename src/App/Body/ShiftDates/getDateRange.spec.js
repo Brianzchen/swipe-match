08a4550d789dfab00e251206c768b83ca96d9665
Brian Chen <brian.chen@tabcorp.com.au>
2020-06-24 20:54:41 +1000

    fix up tests

--- src/App/Body/ShiftDates/getDateRange.spec.js
+++ src/App/Body/ShiftDates/getDateRange.spec.js
@@ -4,8 +4,8 @@ import getDateRange from './getDateRange';
 describe('getDateRange', () => {
   it('returns the correct format', () => {
     expect(getDateRange(
-      '2019-09-05T05:00:00Z',
       '2019-09-04T21:00:00Z',
+      '2019-09-05T05:00:00Z',
     )).toBe('SEP 4, WED 2:00 PM - 10:00 PM PDT');
   });
 });
